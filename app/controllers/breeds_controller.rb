5c461114c916b404bbb6b4f28cca49d1dfef7a9c
archiegabani <archie.gabani@gmail.com>
2024-10-30 14:30:08 -0500

    Was having error saying home/index route does not exists. So added it to routes.rb

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,11 +1,12 @@
 class BreedsController < ApplicationController
   def index
-    @breeds = Breed.all.page(params[:page]).per(9)  # Pagination for all breeds
+    @breeds = Breed.all.page(params[:page]).per(9)  # Fetch all breeds for pagination
 
     if params[:query].present?
+      # Perform the search with case-insensitive matching
       @breeds = Breed.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%").page(params[:page]).per(9)
 
-      # Check if any breeds were found
+      # Check if the search returned any results
       if @breeds.empty?
         @message = "Oops, the breed you entered does not exist in our database!"
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get "pages/about"
-  get "breeds/index"
-  get "about/index"
-  get "home/index"
+  get "pages/about", as: :about  # Define the route for the about page with a named route
 
   # Reveal health status on /up
   get "up" => "rails/health#show", as: :rails_health_check
@@ -14,10 +11,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"  # Keep this if you want home#index as the root
 
-  resources :breeds, only: [:index, :show] do
+  resources :breeds, only: [ :index, :show ] do
     get "subcategories", on: :member  # Nesting the subcategories route here
   end
-
-  # Define the route for the about page
-  get "about", to: "pages#about", as: :about  # Add the `as` option for the named route
 end
